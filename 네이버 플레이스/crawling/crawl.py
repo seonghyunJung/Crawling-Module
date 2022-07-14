b5145f08720be305a6a3c43240d83306bea67285
Ryan <2018113573@dgu.ac.kr>
2022-07-14 19:41:26 +0900

    모두닥 및 네이버 크롤링 모듈 완료

--- 네이버 플레이스/crawling/crawl.py
+++ 네이버 플레이스/crawling/crawl.py
@@ -1,9 +1,9 @@
 from selenium.webdriver.common.by import By
 import time
 
-def naver_place_review(driver, url, hospital_kind, hospital):
+def naver_place_review(driver, hospital):
     try:
-        driver.get(url)
+        # driver.get(url)
         # print('연결성공')
         result = []
         try:
@@ -16,17 +16,22 @@ def naver_place_review(driver, url, hospital_kind, hospital):
             if len(hospitalUrl) == 0:
                 url = 'https://m.place.naver.com/place/list?level=top&entry=pll&x=null&y=null&query=' + hospital
                 driver.get(url)
-                try:
-                    hospitalUrl = driver.find_element(By.XPATH,
-                                                      '//*[@id="_list_scroll_container"]/div/div/div[2]/ul/li/div[2]/a[1][contains(.,' + hospital + ')]')
-                except:
-                    hospitalUrl = driver.find_element(By.XPATH,
-                                                      '//*[@id="_list_scroll_container"]/div/div/div[2]/ul/li/div[2]/a[1]')
+                # try:
+                #     hospitalUrl = driver.find_element(By.XPATH,
+                #                                       '//*[@id="_list_scroll_container"]/div/div/div[2]/ul/li/div[2]/a[1][contains(.,' + hospital + ')]')
+                # except:
+                #     hospitalUrl = driver.find_element(By.XPATH,
+                #                                       '//*[@id="_list_scroll_container"]/div/div/div[2]/ul/li/div[2]/a[1]')
+
+                hospitalUrl = driver.find_element(By.CLASS_NAME, '_3LMxZ')
                 reviewDtail = hospitalUrl.get_attribute('href').split('/')[-1].split('?')[0]
                 reviewUrl = 'https://pcmap.place.naver.com/hospital/' + reviewDtail + '/review/visitor'
-                script = f"window.open('{reviewUrl}');"
-                driver.execute_script(script)
-                driver.switch_to.window(driver.window_handles[-1])
+
+                # script = f"window.open('{reviewUrl}');"
+                # driver.execute_script(script)
+                # driver.switch_to.window(driver.window_handles[-1])
+                driver.get(reviewUrl)
+                time.sleep(2)
                 # time.sleep(3)
                 moreButton = driver.find_elements(By.CSS_SELECTOR, 'div._2kAri')
                 while len(moreButton) > 0:
@@ -43,13 +48,25 @@ def naver_place_review(driver, url, hospital_kind, hospital):
                 except:
                     print('없음')
                 try:
-                    contents = driver.find_elements(By.CSS_SELECTOR, 'li._3l2Wz')
+                    # contents = driver.find_elements(By.CSS_SELECTOR, 'li._3l2Wz')
+                    # for review in contents:
+                    #     content = {
+                    #         'review': review.find_element(By.CSS_SELECTOR, 'div.faZHB').text,
+                    #         'date': review.find_element(By.XPATH, '//div[3]/span[1]/span[2]').text
+                    #     }
+                    #     result.append(content)
+                    contents = driver.find_elements(By.CLASS_NAME, '_3l2Wz')
                     for review in contents:
-                        content = {
-                            'review': review.find_element(By.CSS_SELECTOR, 'div.faZHB').text,
-                            'date': review.find_element(By.XPATH, '//div[3]/span[1]/span[2]').text
-                        }
-                        result.append(content)
+                        try:
+                            content = {
+                                'review': review.find_element(By.CLASS_NAME, 'WoYOw').text,
+                                # 'date': review.find_element(By.XPATH, '//div[3]/span[1]/span[2]').text
+                                # 'date': review.find_element(By.CLASS_NAME, 'utrsf').find_elements(By.CLASS_NAME, 'place_blind')[1].text
+                                'date': review.find_element(By.CLASS_NAME, '_29Yga').find_element(By.CLASS_NAME, 'utrsf').find_elements(By.CLASS_NAME, 'place_blind')[1].text
+                            }
+                            result.append(content)
+                        except:
+                            pass
                 except:
                     print('없음')
                 # print(result)
@@ -76,13 +93,24 @@ def naver_place_review(driver, url, hospital_kind, hospital):
                     except:
                         print('없음')
                     try:
-                        contents = driver.find_elements(By.CSS_SELECTOR, 'li._3l2Wz')
+                        # contents = driver.find_elements(By.CSS_SELECTOR, 'li._3l2Wz')
+                        # for review in contents:
+                        #     content = {
+                        #         'review': review.find_element(By.CSS_SELECTOR, 'div.faZHB').text,
+                        #         'date': review.find_element(By.XPATH, '//div[3]/span[1]/span[2]').text
+                        #     }
+                        #     result.append(content)
+                        contents = driver.find_elements(By.CLASS_NAME, '_3l2Wz')
                         for review in contents:
-                            content = {
-                                'review': review.find_element(By.CSS_SELECTOR, 'div.faZHB').text,
-                                'date': review.find_element(By.XPATH, '//div[3]/span[1]/span[2]').text
-                            }
-                            result.append(content)
+                            try:
+                                content = {
+                                    'review': review.find_element(By.CLASS_NAME, 'WoYOw').text,
+                                    # 'date': review.find_element(By.CLASS_NAME, 'utrsf').find_elements(By.CLASS_NAME, 'place_blind')[1].text
+                                    'date': review.find_element(By.CLASS_NAME, '_29Yga').find_element(By.CLASS_NAME, 'utrsf').find_elements(By.CLASS_NAME, 'place_blind')[1].text
+                                }
+                                result.append(content)
+                            except:
+                                pass
                     except:
                         print('없음')
                     print(result)
--- 네이버 플레이스/crawling/main.py
+++ 네이버 플레이스/crawling/main.py
@@ -5,6 +5,9 @@ import time
 from crawl import *
 import re
 import pandas as pd
+from get_hospitals import get_hospital_list
+from tqdm import tqdm
+
 
 # Globals
 logger = logger.instance
@@ -14,26 +17,59 @@ def main():
     logger.info('Script started.')
     config.init_web()
 
+    
+
     web.start()
     driver = web.driver
     driver.get('https://www.google.com')
-    time.sleep(2)
+    time.sleep(1)
 
-    url = "https://map.naver.com/v5/search/%ED%94%BC%EB%B6%80%EA%B3%BC"
-    hospital = "차앤유의원"
-    hospital_kind = "피부과"
+    # 병원 분과 종류
+    hospital_kind_list = ['치과', '피부과', '성형외과', '산부인과', '정신건강의학과', '비뇨기과','재활의학과', '외과', '소아과', '가정의학과', '한의원']
 
-    data = naver_place_review(driver, url, hospital_kind, hospital)
-    print(data)
+    # hospital_kind_list = ['한의원']
+    hospital_list = {}
+    
+    for hospital_kind in hospital_kind_list:
+        hospital_list[hospital_kind] = get_hospital_list(driver, hospital_kind)
 
-    # 데이터 프레임 생성 및 데이터 입력
-    df = pd.DataFrame(columns = ['분과', '병원명', '리뷰', '날짜'])
-    num = len(data)
 
-    for i in range(num):
-        df.loc[i] = [hospital_kind, hospital, data[i].get('review'), data[i].get('date')]
+    for hospital_kind in hospital_kind_list:
 
-    df.to_excel("test.xlsx", sheet_name = hospital_kind)
+
+        # 데이터 프레임 생성 및 데이터 입력
+        df = pd.DataFrame(columns = ['분과', '병원명', '리뷰', '날짜'])
+        total = 0
+        for hospital in tqdm(hospital_list[hospital_kind]):
+            if (total > 25000):
+                break
+            data = naver_place_review(driver, hospital)
+            num = len(data)
+            
+
+            for i in tqdm(range(num)):
+                df.loc[total] = [hospital_kind, hospital, data[i].get('review'), data[i].get('date')]
+                total += 1
+
+        df.to_excel(f'크롤링 결과/네이버({hospital_kind}).xlsx', sheet_name = hospital_kind)
+
+        
+   
+
+    # hospital_kind = '한의원'
+    # # 데이터 프레임 생성 및 데이터 입력
+    # df = pd.DataFrame(columns = ['분과', '병원명', '리뷰', '날짜'])
+    # hospital = '신곡경희한의원'
+    # data = naver_place_review(driver, hospital)
+    # # print(data)
+    
+    # num = len(data)
+
+    # for i in range(num):
+    #     df.loc[i] = [hospital_kind, hospital, data[i].get('review'), data[i].get('date')]
+
+    # df.to_excel(writer, sheet_name = hospital_kind)
+    # writer.save()
 
 if __name__ == '__main__':
     main()
